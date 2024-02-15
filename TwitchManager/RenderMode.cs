39823dce08f891cc86a4aa29164285a9a04e257f
Giuseppe Terrasi <giuseppe.terrasi@hotmail.it>
2024-02-15 10:55:52 +0100

    Add RenderMode InteractiveServerNotPrerendered

---
+++ TwitchManager/RenderMode.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace TwitchManager
+{
+    public static class RenderMode
+    {
+        public static InteractiveServerRenderMode InteractiveServerNotPrerendered { get; } = new(false);
+    }
+}
